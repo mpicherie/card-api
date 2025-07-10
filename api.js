baecf136a10e981f39bc1e3cc88760c5a71b6844
Paozie83 <paozie83490@gmail.com>
2025-07-10 21:06:51 +0200

    Fix Puppeteer build for Render

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
